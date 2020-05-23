587e129d29f9fb3ddcd94de4c5f1d894d8ed750e
Gary Mullen <garyfmullen@gmail.com>
2020-05-23 11:25:13 -0700

    Displaying Card Image

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ A discord bot for getting MTG Cards in chat
 **I want to be able to not have to type the exact name**
 
 API Used: https://docs.magicthegathering.io/
+
 SDK Used: https://github.com/MagicTheGathering/mtg-sdk-python
+
 2. Display Card Images
 3. Display Card Test
 4. Integrate to Discor
--- boltbot.py
+++ boltbot.py
@@ -2,6 +2,9 @@
 import os.path
 import requests as req
 import json
+from io import BytesIO
+from PIL import Image
+
 from mtgsdk import Card
 from mtgsdk import Set
 
@@ -15,11 +18,19 @@ TEST_SET = "IKO"
 
 def main():
     print(GREEN + "BoltBot" + ENDC)
-    card = Card.where(set='m11').where(name=TEST_CARD).all()
+    #all_cards = Card.all()
+
+    card = Card.where(name=TEST_CARD).all()
+
     print(GREEN + "Card Found: " + str(card[0].name))
     print(GREEN + "Mana Cost: " + str(card[0].mana_cost))
     print(GREEN + "Type:  " + str(card[0].type))
     print(GREEN + "Text:  " + str(card[0].text))
+    print(GREEN + "Card URL: " + str(card[0].image_url))
+    
+    get_image = req.get(card[0].image_url)
+    card_image = Image.open(BytesIO(get_image.content))
+    card_image.show()
 
 if __name__ == "__main__":
     main()
